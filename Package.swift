d463534985fd9cc58f7ee0386a577a646a7f8c5a
jackie-eb <yinbo@easternbay.cn>
2021-01-28 20:20:08 +0800

    remove platforms set info

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "CalendarControl",
-    platforms: [
-      .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
-    ],
     products: [
       .library(
         name: "CalendarControl",
